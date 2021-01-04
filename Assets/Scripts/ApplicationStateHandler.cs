af096641410c2f9db216b7dc33fc4ca0aedef9e4
NAL <niko.a.leinonen@gmail.com>
2021-01-04 14:24:34 +0200

    Code cleanup

--- Assets/Scripts/ApplicationStateHandler.cs
+++ Assets/Scripts/ApplicationStateHandler.cs
@@ -20,27 +20,20 @@ public class ApplicationStateHandler : MonoBehaviour
     public GameHandler gameHandler;
 	public TabGroup tabGroup;
 	public DownloadsMenu downloadsMenu;
-#if UNITY_EDITOR
-	public bool EditorOverride;
-	public int overrideFPS;
-#endif
 
 	void OnApplicationFocus(bool hasFocus)
 	{
-#if UNITY_EDITOR
-			if (EditorOverride)
-			{
-				Application.targetFrameRate = overrideFPS;
-				return;
-			}
-#endif
+		#region Focused
 		if (hasFocus)
 			Application.targetFrameRate = -1;
+		#endregion
+		#region Unfocused
 		else if (tabGroup.SelectedButton == tabGroup.tabButtons[0])
 			Application.targetFrameRate = 30;
 		else if (!downloadsMenu.trueOpened || gameHandler.GetActiveData() != null)
 			Application.targetFrameRate = 1;
 		else
 			Application.targetFrameRate = Mathf.CeilToInt(1 / Time.fixedUnscaledDeltaTime);
+		#endregion
 	}
 }
