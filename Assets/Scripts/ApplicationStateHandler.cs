0fa34d7dd1217664a630b69cbf40349556706506
NAL <niko.a.leinonen@gmail.com>
2021-01-04 14:17:43 +0200

    Better FPS handling

--- Assets/Scripts/ApplicationStateHandler.cs
+++ Assets/Scripts/ApplicationStateHandler.cs
@@ -19,6 +19,7 @@ public class ApplicationStateHandler : MonoBehaviour
 {
     public GameHandler gameHandler;
 	public TabGroup tabGroup;
+	public DownloadsMenu downloadsMenu;
 #if UNITY_EDITOR
 	public bool EditorOverride;
 	public int overrideFPS;
@@ -35,7 +36,9 @@ public class ApplicationStateHandler : MonoBehaviour
 #endif
 		if (hasFocus)
 			Application.targetFrameRate = -1;
-		else if (gameHandler.GetActiveData() != null)
+		else if (tabGroup.SelectedButton == tabGroup.tabButtons[0])
+			Application.targetFrameRate = 30;
+		else if (!downloadsMenu.trueOpened || gameHandler.GetActiveData() != null)
 			Application.targetFrameRate = 1;
 		else
 			Application.targetFrameRate = Mathf.CeilToInt(1 / Time.fixedUnscaledDeltaTime);
